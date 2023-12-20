9f705b23d3ef6532f0d2b568ff89f02ee91fbe28
Blake McBride <blake@mcbridemail.com>
2023-12-20 02:29:04 -0600

    Update GC test code

--- examples/exam09/main.c
+++ examples/exam09/main.c
@@ -11,16 +11,16 @@ int main(int argc, char *argv[])
 
 
 	/*  The following line is used to turn on the automatic garbage
-	    collector (un-comment after the first run) */
-
-	gSetMemoryBufferArea(Dynace, 50000L);   
+	    collector. Try this program with the following line commented
+	    and uncommented to see the difference the garbage collector makes. */
+	//gSetMemoryBufferArea(Dynace, 50000L);   
 
 
 
 	/*  Create enough new objects to fill up all the memory of a PC
 	    (since no objects are ever explictly disposed).  */
 
-	for (n=0 ; n++ != 125000L ; )  {
+	for (n=0 ; n++ != 1250000L ; )  {
 		if (!(n % 1000L))
 			printf("%ld\n", n);
 		obj = gNewWithDouble(DoubleFloat, 3.141);
